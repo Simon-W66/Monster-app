258b03ba7b7a14a5834404b9a3815564bad0bc29
unknown <patrixstar565@gmail.com>
2020-12-18 12:24:39 +0100

    adding files for github pages

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+
+import {CardList} from './components/card-list/card-list.components';
+
+import './App.css';
+
+import { SearchBox } from "./components/search-box/search-box.component.jsx"
+
+class App extends Component {
+  constructor(){
+    
+   super();
+
+    this.state = {
+     monsters: [],
+     searchField: '',
+   }
+   this.handleChange = this.handleChange.bind(this) 
+  }
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users=> this.setState({monsters: users}))
+  }
+  handleChange(e){
+    this.setState({searchField: e.target.value})
+  }
+   
+  render() { 
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(
+      monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return ( 
+      <section className="App">
+      <h1 className="header">Monster karusel</h1>
+      <SearchBox 
+        placeholder='search monsters'
+        handleChange={this.handleChange}
+      />
+        <CardList monsters={filteredMonsters}/>
+      </section>
+     );
+  }
+}
+ 
+export default App;
+
+
+//componentDidMount(){
+//    fetch("https://jsonplaceholder.typicode.com/users")
+//    .then(apple=> apple.json())
+//    .then(email=> this.setState({monsters:email}))
+//  }
